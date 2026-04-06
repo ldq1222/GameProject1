f06dbd90be2c7f6aa50d5a2c82117a7ade4bd65a
ldq1222 <14152276@qq.com>
2026-04-06 13:21:53 +0800

    添加项目文件。

---
+++ src/Player.h
@@ -0,0 +1,43 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+//initialize=ChuShiHua
+//init() is better than Player() because it
+//can load the Player multiple times 
+class Player
+{	
+private:
+	sf::Sprite sprite;
+	sf::Texture texture;
+
+	sf::Vector2f position;
+	const float speedCommon = 10.0f;
+	const float speedJump = 2.0f;
+	const float speedDash = 100.0f;
+	const float speedClimb = 5.0f;
+	const float weight = 20.0f;
+	const double lengthCanDash = 40.0f;
+	int directionX;
+	int directionY;
+	sf::Vector2f speedCurrent;
+	bool isDashing;
+	bool isJumping;
+	bool isWalking;
+	bool isFalling;
+	int countJump;
+	double lengthDash;
+public:
+	Player();
+	//auto gets called out when created
+	~Player();
+	//auto gets called out when deleated
+	
+	void init(std::string texture_name, sf::Vector2f pos, float gravity);
+	
+	void walk(int dir);
+	void climb(int dir);
+	void fall();
+	void dash(int dir);
+	void jump();
+	void update(double deltatime);
+	sf::Sprite getSprite();
+};
---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+﻿#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <time.h>
+#include <iostream>
+#include "Player.h"
+Player player;
+void update(double deltatime) {
+    player.update(deltatime);
+}
+int main() {
+
+    sf::Window window;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "sth");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            ////INPUT////
+            if (event.type == sf::Keyboard::A) {
+                player.walk(-1);
+            }
+            if (event.type == sf::Keyboard::D) {
+                player.walk(1);
+            }
+            if (event.type == sf::Keyboard::Backspace) {
+                player.jump();
+            }
+        }
+        window.clear();
+        window.draw(player.getsprite());
+    }
+    
+	return 0;
+}
